d806dcfb25eac1a01457f63375f9d67d128e6540
Ankur Oberoi <aoberoi@gmail.com>
2017-11-22 06:58:48 -0500

    use protocol extensions and closures

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -14,17 +14,7 @@ struct Concentration {
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil;
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -55,5 +45,11 @@ struct Concentration {
             cards += [card, card]
         }
         // TODO: shuffle the cards
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
